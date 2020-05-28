477d6f3c26ffe7eec60e3e2d56a34f7605464897
王漩波 <1345545983@qq.com>
2020-05-28 17:57:37 +0800

    [A] 监听换成jackson序列化，防止autoType问题 & CacheMessage无默认构造器无法反序列化问题

--- modules/egova-framework-cache/src/main/java/com/egova/cache/CacheMessageListener.java
+++ modules/egova-framework-cache/src/main/java/com/egova/cache/CacheMessageListener.java
@@ -1,6 +1,6 @@
 package com.egova.cache;
 
-import com.alibaba.fastjson.JSONObject;
+import com.egova.json.JsonMapping;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
@@ -12,22 +12,24 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Slf4j
 public class CacheMessageListener implements MessageListener {
 
-    private RedisTemplate<Object, Object> redisTemplate;
+    private final RedisTemplate<Object, Object> redisTemplate;
 
-    private RedisEhcacheCacheManager redisEhcacheCacheManager;
+    private final RedisEhcacheCacheManager redisEhcacheCacheManager;
 
+    private final JsonMapping jsonMapping;
 
-    public CacheMessageListener(RedisTemplate<Object, Object> redisTemplate, RedisEhcacheCacheManager redisEhcacheCacheManager) {
+    public CacheMessageListener(RedisTemplate<Object, Object> redisTemplate, RedisEhcacheCacheManager redisEhcacheCacheManager, JsonMapping jsonMapping) {
         super();
         this.redisTemplate = redisTemplate;
         this.redisEhcacheCacheManager = redisEhcacheCacheManager;
+        this.jsonMapping = jsonMapping;
     }
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
         CacheMessage cacheMessage;
         try {
-            cacheMessage = JSONObject.parseObject(message.getBody(), CacheMessage.class);
+            cacheMessage = jsonMapping.deserialize(new String(message.getBody()), CacheMessage.class);
             log.debug(String.format("receive a redis topic message, clear local cache, the cacheName is %s, the key is %s", cacheMessage.getCacheName(), cacheMessage.getKey()));
             redisEhcacheCacheManager.clearLocal(cacheMessage.getCacheName(), cacheMessage.getKey(), cacheMessage.getSender());
         } catch (Exception e) {
