a1fed411abc6b96e9ad10cd621a14783157fdb74
王漩波 <1345545983@qq.com>
2020-07-01 14:52:16 +0800

    [M] oauth2 client设置服务发现

--- modules/egova-framework-cloud/src/main/java/com/egova/cloud/oauth2/OAuth2ClientTokenConfiguration.java
+++ modules/egova-framework-cloud/src/main/java/com/egova/cloud/oauth2/OAuth2ClientTokenConfiguration.java
@@ -1,12 +1,16 @@
 package com.egova.cloud.oauth2;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.common.AuthenticationScheme;
+
+import java.util.Collections;
 
 /**
  * @author chendb
@@ -16,9 +20,8 @@ import org.springframework.security.oauth2.common.AuthenticationScheme;
 @EnableConfigurationProperties(OAuth2ClientProperties.class)
 public class OAuth2ClientTokenConfiguration {
 
-    @LoadBalanced
     @Bean("egovaOAuth2RestTemplate")
-    public OAuth2RestTemplate oAuth2RestTemplate(OAuth2ClientProperties oAuth2ClientProperties) {
+    public OAuth2RestTemplate oAuth2RestTemplate(OAuth2ClientProperties oAuth2ClientProperties, LoadBalancerClient loadBalancer) {
         ClientCredentialsResourceDetails details = new ClientCredentialsResourceDetails();
         details.setId(oAuth2ClientProperties.getId());
         details.setAccessTokenUri(oAuth2ClientProperties.getAccessTokenUrl());
@@ -26,7 +29,12 @@ public class OAuth2ClientTokenConfiguration {
         details.setClientSecret(oAuth2ClientProperties.getClientSecret());
         details.setAuthenticationScheme(AuthenticationScheme.valueOf(oAuth2ClientProperties.getClientAuthenticationScheme()));
         // 注意OAuth2ClientContext不能从spring上下文中注入进来，因为默认OAuth2ClientContext是@Scope为 "session" ,在子线程中会重新创建bean失败
-        return new OAuth2RestTemplate(details, new DefaultOAuth2ClientContext());
+        OAuth2RestTemplate template = new OAuth2RestTemplate(details, new DefaultOAuth2ClientContext());
+        // 手动设置负载均衡
+        ClientCredentialsAccessTokenProvider provider = new ClientCredentialsAccessTokenProvider();
+        provider.setInterceptors(Collections.singletonList(new LoadBalancerInterceptor(loadBalancer)));
+        template.setAccessTokenProvider(provider);
+        return template;
     }
 
 }
