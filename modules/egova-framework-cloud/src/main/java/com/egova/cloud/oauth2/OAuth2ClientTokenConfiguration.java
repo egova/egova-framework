2224a3b9f12748be488e613fae122ab2be149d77
王漩波 <1345545983@qq.com>
2020-07-01 14:08:16 +0800

    [M] oauth2 client服务发现

--- modules/egova-framework-cloud/src/main/java/com/egova/cloud/oauth2/OAuth2ClientTokenConfiguration.java
+++ modules/egova-framework-cloud/src/main/java/com/egova/cloud/oauth2/OAuth2ClientTokenConfiguration.java
@@ -1,6 +1,7 @@
 package com.egova.cloud.oauth2;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
@@ -15,10 +16,9 @@ import org.springframework.security.oauth2.common.AuthenticationScheme;
 @EnableConfigurationProperties(OAuth2ClientProperties.class)
 public class OAuth2ClientTokenConfiguration {
 
-
+    @LoadBalanced
     @Bean("egovaOAuth2RestTemplate")
     public OAuth2RestTemplate oAuth2RestTemplate(OAuth2ClientProperties oAuth2ClientProperties) {
-
         ClientCredentialsResourceDetails details = new ClientCredentialsResourceDetails();
         details.setId(oAuth2ClientProperties.getId());
         details.setAccessTokenUri(oAuth2ClientProperties.getAccessTokenUrl());
@@ -26,7 +26,7 @@ public class OAuth2ClientTokenConfiguration {
         details.setClientSecret(oAuth2ClientProperties.getClientSecret());
         details.setAuthenticationScheme(AuthenticationScheme.valueOf(oAuth2ClientProperties.getClientAuthenticationScheme()));
         // 注意OAuth2ClientContext不能从spring上下文中注入进来，因为默认OAuth2ClientContext是@Scope为 "session" ,在子线程中会重新创建bean失败
-        final OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(details, new DefaultOAuth2ClientContext());
-        return oAuth2RestTemplate;
+        return new OAuth2RestTemplate(details, new DefaultOAuth2ClientContext());
     }
+
 }
