244ca07c3ab5af88d7180847dcdc844e4461f4d3
王漩波 <1345545983@qq.com>
2020-08-03 14:12:37 +0800

    [M] 防止前端日期组件取消日期之后，仍传递空字符串问题

--- modules/egova-framework-jackson/src/main/java/com/egova/json/databind/std/CustomTimestampDeseralizer.java
+++ modules/egova-framework-jackson/src/main/java/com/egova/json/databind/std/CustomTimestampDeseralizer.java
@@ -4,14 +4,16 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonTokenId;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.UntypedObjectDeserializer;
+import com.flagwind.commons.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.Date;
 
 /**
  * 自定义时间解析器
- * 
+ *
  * @author chendb
  * @date 2016年12月8日 下午11:35:36
  */
@@ -19,26 +21,26 @@ import java.sql.Timestamp;
 public class CustomTimestampDeseralizer extends UntypedObjectDeserializer {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 8457240513909259495L;
 
     @Override
     public Object deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
-
         if (jp.getCurrentTokenId() == JsonTokenId.ID_STRING) {
-            try {
-
-               java.util.Date date = DateUtils.parseDate(jp.getText(), "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm",
-                        "yyyy-MM-dd HH","yyyy-MM-dd", "yyyy-MM-dd'T'HH:mm:ss.SSSZ","yyyy-MM-dd'T'HH:mm:ss.SSSXXX", "yyyy-MM-dd'T'HH:mm:ss");
-
-               return new Timestamp(date.getTime());
+            // 防止前端日期组件取消日期之后，传递空字符串问题，这里返回null
+            if (StringUtils.isBlank(jp.getText())) {
+                return null;
             }
-            catch (Exception e) {
+            try {
+                Date date = DateUtils.parseDate(jp.getText(), "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm",
+                        "yyyy-MM-dd HH", "yyyy-MM-dd", "yyyy-MM-dd'T'HH:mm:ss.SSSZ", "yyyy-MM-dd'T'HH:mm:ss.SSSXXX", "yyyy-MM-dd'T'HH:mm:ss");
+
+                return new Timestamp(date.getTime());
+            } catch (Exception e) {
                 return super.deserialize(jp, ctxt);
             }
-        }
-        else {
+        } else {
             return super.deserialize(jp, ctxt);
         }
     }
